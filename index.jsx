3eaa932ef9b7ec3b8f7925175b1a5bc105b617c0
AAGaming00 <aa@bigdumb.gq>
2020-11-01 18:29:16 -0500

    yes

--- index.jsx
+++ index.jsx
@@ -1,27 +1,25 @@
-const { getModuleByDisplayName } = require('@webpack');
-const { patch, unpatch } = require('@patcher');
-const { joinClassNames } = require('@util');
-const { Tooltip } = require('@components');
-const { Plugin } = require('@entities');
-const { React } = require('@react');
+const { getModuleByDisplayName, React } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
+const { Tooltip } = require('powercord/components');
+const { Plugin } = require('powercord/entities');
 
 const SpotifyLogo = '/assets/f0655521c19c08c4ea4e508044ec7d8c.png';
 const TwitchLogo = '/assets/edbbf6107b2cd4334d582b26e1ac786d.png';
 
 module.exports = class ChannelMembersActivityIcons extends Plugin {
   onStart () {
-    this.injectStyles('styles/main.scss');
+    this.loadStylesheet('./styles/main.scss');
     this.injectActivityIcons();
   }
 
   onStop () {
-    unpatch('channel-members-activity-icons');
+    uninject('channel-members-activity-icons');
   }
 
   async injectActivityIcons () {
     const MemberListItem = getModuleByDisplayName('MemberListItem');
 
-    patch('channel-members-activity-icons', MemberListItem.prototype, 'render', (_, res) => {
+    inject('channel-members-activity-icons', MemberListItem.prototype, 'render', (_, res) => {
       if (!res.props || !res.props.subText || !res.props.subText.props) return res;
 
       const { activities } = res.props.subText.props;
@@ -45,7 +43,7 @@ module.exports = class ChannelMembersActivityIcons extends Plugin {
               />
             </Tooltip>;
 
-          res.props.className = joinClassNames(res.props.className, 'vz-hasActivityIcon');
+          res.props.className = `${res.props.className} vz-hasActivityIcon`;
 
           return res;
         }
